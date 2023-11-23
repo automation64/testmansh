e5b3a7e6e4950f4cac1cd1750a730d1811de9f00
serdigital64 <serdigital64@gmail.com>
2023-11-23 15:22:03 +0000

    refact: upgrade bl64

--- src/main.bash
+++ src/main.bash
@@ -3,9 +3,7 @@
 #
 
 # Local variables
-declare testmansh_status=1
 declare testmansh_command=''
-declare testmansh_command_tag=''
 declare testmansh_option=''
 declare testmansh_case='all'
 declare testmansh_debug_test="$BL64_VAR_OFF"
@@ -18,30 +16,12 @@ declare testmansh_verbose="$BL64_MSG_VERBOSE_APP"
 (($# == 0)) && testmansh_help && exit 1
 while getopts ':tbliokqs:p:c:e:r:f:m:u:gj:V:D:h' testmansh_option; do
   case "$testmansh_option" in
-  t)
-    testmansh_command='testmansh_run_linter'
-    testmansh_command_tag='run shellcheck linter'
-    ;;
-  b)
-    testmansh_command='testmansh_run_test'
-    testmansh_command_tag='run bats-core tests'
-    ;;
-  l)
-    testmansh_command='testmansh_list_test_scope'
-    testmansh_command_tag='list test cases for bats-core'
-    ;;
-  i)
-    testmansh_command='testmansh_list_images'
-    testmansh_command_tag='list container images'
-    ;;
-  k)
-    testmansh_command='testmansh_list_linter_scope'
-    testmansh_command_tag='list source code files for shellcheck'
-    ;;
-  q)
-    testmansh_command='testmansh_open_container'
-    testmansh_command_tag='open testing container'
-    ;;
+  t) testmansh_command='testmansh_run_linter' ;;
+  b) testmansh_command='testmansh_run_test' ;;
+  l) testmansh_command='testmansh_list_test_scope' ;;
+  i) testmansh_command='testmansh_list_images' ;;
+  k) testmansh_command='testmansh_list_linter_scope' ;;
+  q) testmansh_command='testmansh_open_container' ;;
   m) testmansh_format="$OPTARG" ;;
   j) testmansh_report="$OPTARG" ;;
   p) TESTMANSH_PROJECT="$OPTARG" ;;
@@ -61,16 +41,11 @@ while getopts ':tbliokqs:p:c:e:r:f:m:u:gj:V:D:h' testmansh_option; do
 done
 testmansh_initialize "$testmansh_debug" "$testmansh_verbose" "$testmansh_container" "$testmansh_command" || exit 1
 
-bl64_msg_show_batch_start "$testmansh_command_tag"
+bl64_msg_show_batch_start "$testmansh_command"
 case "$testmansh_command" in
 'testmansh_list_test_scope' | 'testmansh_list_images' | 'testmansh_open_container' | 'testmansh_list_linter_scope') "$testmansh_command" ;;
 'testmansh_run_linter') "$testmansh_command" "$testmansh_container" "$testmansh_format" "$testmansh_report" "$testmansh_case" ;;
 'testmansh_run_test') "$testmansh_command" "$testmansh_container" "$testmansh_format" "$testmansh_report" "$testmansh_debug_test" "$testmansh_case" ;;
 *) bl64_check_alert_parameter_invalid "$testmansh_command" ;;
 esac
-testmansh_status=$?
-
-# shellcheck disable=SC2248
-bl64_msg_show_batch_finish $testmansh_status "$testmansh_command_tag"
-# shellcheck disable=SC2248
-exit $testmansh_status
+bl64_msg_show_batch_finish $? "$testmansh_command"
